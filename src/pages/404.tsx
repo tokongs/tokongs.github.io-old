67b3e783fc9b4bd7f557f4c51e5648d7b53b701e
Tobias Kongsvik <tokongs98@gmail.com>
2020-08-17 23:39:12 +0200

    simplify

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,11 +3,11 @@ import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
+const NotFoundPage: React.FC = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Not found" />
+    <h1>Not found</h1>
+    <p>This path leads nowhere, get back!</p>
   </Layout>
 )
 
